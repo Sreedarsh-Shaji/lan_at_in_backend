3889093057ab0402d62cb1116455f365b70ebc9e
Sreedarsh Shaji <tpzkollam@gmail.com>
2022-05-04 23:44:02 +0530

    Updated index

--- src/main/java/com/jobseeker/company/jobseekercompany/controller/InfoController.java
+++ src/main/java/com/jobseeker/company/jobseekercompany/controller/InfoController.java
@@ -17,7 +17,7 @@ public class InfoController {
                 "<table>" +
                 "<tr><th>GIT</th><td> : </td><td>   https://github.com/Sreedarsh-Shaji/Placement_portal_snit.git</td></tr>" +
                 "<tr><th>Heroku URL</th><td> : </td><td>https://serene-castle-69736.herokuapp.com/</td></tr>" +
-                "<tr><th>Swagger URL</th><td> : </td><td><a href ='/swagger-ui/index.html'>Link</a></td></tr>" +
+                "<tr><th>Swagger URL</th><td> : </td><td><a href ='/swagger-ui.html'>Link</a></td></tr>" +
                 "</table>" +
                 "</center>";
     }
